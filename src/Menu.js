a5301ee5e60a7ddd4bd156bc349d543cd4b70692
tom bina <tom.bina@divide.nl>
2019-06-25 19:58:45 +0200

    building your first React component

---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Customers</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Menu;
