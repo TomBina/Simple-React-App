8f89ef494f3f4807f7503811663c1fa3de1a3bd4
tom bina <tom.bina@divide.nl>
2019-07-13 11:40:45 +0200

    understanding state and lifecycle

--- src/Customers.js
+++ src/Customers.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import CustomerCard from './CustomerCard';
 
-function Customers() {
-    return (
-        <div>
-            <h1>Customers</h1>
-            <CustomerCard 
-                company="Company ltd." 
-                address="177 Arnold Ave." 
-                city="Waxhaw, NC 28173">
-            </CustomerCard>
-        </div>
-    )
+class Customers extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            customers: []
+        };
+    }
+
+    render() {
+        let cards = this.state.customers.map(c => <CustomerCard
+            key={c.id}
+            company={c.company}
+            address={c.address}
+            city={c.city}>
+        </CustomerCard>)
+
+        return (
+            <div>
+                <h1>Customers</h1>
+                {cards.length > 0 ? cards : "loading.."}
+            </div>
+        )
+    }
+
+    async componentDidMount() {
+        let customers = await fetch("/customers.json").then(response => response.json());
+
+        this.setState({
+            customers: customers
+        });
+    }
+
+    async componentWillUnmount() {
+        console.log("Component will be removed.");
+    }
 }
 
 export default Customers;
