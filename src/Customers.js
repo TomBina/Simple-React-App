ba115db976d0f8c1062449002933658c6625f5dc
Tom Bina <31348448+TomBina@users.noreply.github.com>
2019-07-23 20:27:14 +0200

    working with events

--- src/Customers.js
+++ src/Customers.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CustomerCard from './CustomerCard';
+import './Customers.css';
 
 function Customers() {
+    let searchElement = useRef();
     let [customers, setCustomers] = useState([]);
+    let [query, setQuery] = useState("");
+    let [message, setMessage] = useState("Loading..");
     let cards = customers.map(c => <CustomerCard
         key={c.id}
         company={c.company}
@@ -11,18 +15,37 @@ function Customers() {
     </CustomerCard>)
 
     useEffect(() => {
+        searchElement.current.focus();
+    }, []);
+
+    useEffect(() => {
         async function getCustomers() {
             let customers = await fetch("/customers.json").then(response => response.json());
+
+            if (!query) {
+                setMessage(null);
+                setCustomers(customers);
+                return;
+            }
+
+            customers = customers.filter(c => c.company.toLowerCase().startsWith(query.toLowerCase()));
+
+            if (customers.length === 0) {
+                setMessage("No results found.");
+                return;
+            }
+
             setCustomers(customers);
         }
 
         getCustomers();
-    }, []);
-    
+    }, [query]);
+
     return (
         <div>
             <h1>Customers</h1>
-            {cards.length > 0 ? cards : "loading.."}
+            <input ref={searchElement} value={query} onChange={(e) => setQuery(e.target.value)} placeholder="search" className="customers-query" />
+            {message ? message : cards}
         </div>
     );
 }
