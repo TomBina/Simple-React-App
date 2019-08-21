d27c06257c74891ec65031273b0bad36368adc7a
tom bina <tom.bina@divide.nl>
2019-08-21 07:26:02 +0200

    redesign

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Menu from "./Menu";
-import './App.css';
 
 function App() {
   return (
--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Menu.css";
 
 function Menu() {
     return (
