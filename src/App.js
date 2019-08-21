cf07b165e7cc0b524a4a5b3ad9b015933da6fcf5
tom bina <tom.bina@divide.nl>
2019-08-21 21:32:33 +0200

    update homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 import Menu from "./Menu";
 
 function App() {
   return (
     <div>
-      <Menu></Menu>
-      <h1>Simple React CRM app</h1>
-      <p>This is an example of a simple CRM app written in React.</p>
+      <Menu />
+      <h1>Simple React app</h1>
+      <p>This is an example of an app written in React. During the course, we will add many features like:</p>
+      <ul>
+        <li>Sign in/sign up.</li>
+        <li>Add, update, and delete customers.</li>
+        <li>Search customers</li>
+        <li>Persist data in Cloud Firestore.</li>
+        <li>Log user actions.</li>
+      </ul>
+      <p>
+        Click <a href="https://www.coolbytes.io/a-complete-guide-to-react" target="_blank">here</a> to view all lessons.
+      </p>
+      <p>Licensed under MIT.</p>
     </div>
   );
 }
