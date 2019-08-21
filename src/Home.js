d4187b3b2119918b9d8ede54358a8290c61f356e
tom bina <tom.bina@divide.nl>
2019-08-21 21:36:23 +0200

    update homepage + prettier

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return (<div>
-        <h1>Simple React CRM app</h1>
-        <p>This is an example of a simple CRM app written in React.</p>
-    </div>)
+    return (
+        <div>
+            <h1>Simple React app</h1>
+            <p>This is an example of an app written in React. During the course, we will add many features like:</p>
+            <ul>
+                <li>Sign in/sign up.</li>
+                <li>Add, update, and delete customers.</li>
+                <li>Search customers</li>
+                <li>Persist data in Cloud Firestore.</li>
+                <li>Log user actions.</li>
+            </ul>
+            <p>
+                Click{" "}
+                <a href="https://www.coolbytes.io/a-complete-guide-to-react" target="_blank">
+                    here
+                </a>{" "}
+                to view all lessons.
+            </p>
+            <p>Licensed under MIT.</p>
+        </div>
+    );
 }
 
 export default Home;
