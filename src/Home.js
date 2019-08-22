ac8709aff3e99aade7689d7d0916b59a9fa0a901
tom bina <tom.bina@divide.nl>
2019-08-22 07:42:49 +0200

    bg + react logo

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
+import logo from "./logo.svg";
 
 function Home() {
     return (
         <div>
             <h1>Simple React app</h1>
+            <img src={logo} width="200" />
             <p>This is an example of an app written in React. During the course, we will add many features like:</p>
             <ul>
                 <li>Sign in/sign up.</li>
                 <li>Add, update, and delete customers.</li>
-                <li>Search customers</li>
+                <li>Search customers.</li>
                 <li>Persist data in Cloud Firestore.</li>
                 <li>Log user actions.</li>
             </ul>
